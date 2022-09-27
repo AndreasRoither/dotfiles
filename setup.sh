c9fdd8aebfbe9bf61a4e98b69871f76c78cb612d
Andreas Roither <andi.roither@protonmail.com>
2022-09-27 11:59:35 +0200

    Fix missing fi

--- setup.sh
+++ setup.sh
@@ -17,18 +17,18 @@ declare skipQuestions=false
 # ----------------------------------------------------------------------
 
 get_os() {
-    unameOut="$(uname -s)"
-    case "${unameOut}" in
-        Linux*)     os=Linux;;
-        Darwin*)    os=Mac;;
-        CYGWIN*)    os=Cygwin;;
-        MINGW*)     os=MinGw;;
-        *)          os="UNKNOWN:${unameOut}"
-    esac
-    
-    if [ $os == "Linux" ]; then
-      distro=$(lsb_release -si)
-    fi
+  unameOut="$(uname -s)"
+  case "${unameOut}" in
+  Linux*) os=Linux ;;
+  Darwin*) os=Mac ;;
+  CYGWIN*) os=Cygwin ;;
+  MINGW*) os=MinGw ;;
+  *) os="UNKNOWN:${unameOut}" ;;
+  esac
+
+  if [ $os == "Linux" ]; then
+    distro=$(lsb_release -si)
+  fi
 }
 
 indent() { sed 's/^/    /'; }
@@ -39,75 +39,74 @@ indent() { sed 's/^/    /'; }
 
 main() {
 
-    get_os
+  get_os
 
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-    # Load utils
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  # Load utils
 
-    if [ -x "src/util/utils.sh" ]; then
-        . "src/util/utils.sh" || exit 1
-    else
-      echo "Error loading utils."
+  if [ -x "src/util/utils.sh" ]; then
+    . "src/util/utils.sh" || exit 1
+  else
+    echo "Error loading utils."
+    exit 1
+  fi
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  print_in_purple "\n • Dotfiles install starting\n"
+
+  ask_for_sudo
+
+  # Make sure gum is installed
+  if [ "$os" == "MinGw" ]; then
+    print_in_blue "\tInstalling gum\n"
+    start microsoft-edge:https://github.com/charmbracelet/gum/releases/download/v0.6.0/gum_0.6.0_Windows_x86_64.zip
+    echo "Waiting for you to install gum manually"
+    echo "Press any key if complete..."
+    read -n 1 -s
+
+  elif [ "$os" == "Linux" ]; then
+    if [ "$distro" == "Ubuntu" ]; then
+      echo 'deb [trusted=yes] https://repo.charm.sh/apt/ /' | sudo tee /etc/apt/sources.list.d/charm.list && sudo apt update && sudo apt install gum
+    elif [ "$os" == "ManjaroLinux" ]; then
+      pacman -S gum
+    fi
+  fi
+
+  # update dotbot
+  gum spin --spinner dot --title "Updating dotbot" -- git -C "${DOTBOT_DIR}" submodule sync --quiet --recursive && git submodule update --init --recursive "${DOTBOT_DIR}"
+  gum confirm "Create directories?" && bash ./src/preferences/create_directories.sh
+
+  print_in_purple "\n • Starting dotbot...\n\n"
+  # start dotbot actions
+  "${BASEDIR}/${DOTBOT_DIR}/${DOTBOT_BIN}" -d "${BASEDIR}" -c "${CONFIG}" "${@}" | indent
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  skip_questions "$@" &&
+    skipQuestions=true
+
+  # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+  print_in_purple "\n • Looking for install scripts\n\n"
+
+  if [ "$os" == "MinGw" ]; then
+    print_in_blue "\tInstalling windows dependencies\n"
+    bash ./src/os/windows/dep-win10-winget.sh
+
+  elif [ "$os" == "Linux" ]; then
+
+    if [ ! -d "src/os/${distro}" ]; then
+      print_error "Could not find installation files for '${distro}'."
       exit 1
     fi
 
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+    bash ./src/os/${distro}/setup.sh
 
-    print_in_purple "\n• Dotfiles install starting\n"
-
-    ask_for_sudo
-
-    # Make sure gum is installed
-    if [ "$os" == "MinGw" ]; then
-      print_in_blue "\tInstalling gum\n"
-      start microsoft-edge:https://github.com/charmbracelet/gum/releases/download/v0.6.0/gum_0.6.0_Windows_x86_64.zip
-      echo "Waiting for you to install gum manually"
-      echo "Press any key if complete..."
-      read -n 1 -s
-
-    elif [ "$os" == "Linux" ]; then
-      if [ "$distro" == "Ubuntu" ]; then
-        echo 'deb [trusted=yes] https://repo.charm.sh/apt/ /' | sudo tee /etc/apt/sources.list.d/charm.list && sudo apt update && sudo apt install gum
-      elif [ "$os" == "ManjaroLinux" ]; then
-        pacman -S gum
-    fi
-
-    # update dotbot
-    gum spin --spinner dot --title "Updating dotbot" -- git -C "${DOTBOT_DIR}" submodule sync --quiet --recursive && git submodule update --init --recursive "${DOTBOT_DIR}"
-    gum confirm "Create directories?" && bash ./src/preferences/create_directories.sh
-
-
-    print_in_purple "\n• Starting dotbot...\n\n"
-    # start dotbot actions
-    "${BASEDIR}/${DOTBOT_DIR}/${DOTBOT_BIN}" -d "${BASEDIR}" -c "${CONFIG}" "${@}" | indent
-    
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    skip_questions "$@" \
-        && skipQuestions=true
-
-    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-    print_in_purple "\n• Looking for install scripts\n\n"
-
-    if [ "$os" == "MinGw" ]; then
-      print_in_blue "\tInstalling windows dependencies\n"
-      bash ./src/os/windows/dep-win10-winget.sh
-
-    elif [ "$os" == "Linux" ]; then
-
-      if [ ! -d "src/os/${distro}" ]; then
-        print_error "Could not find installation files for '${distro}'."
-        exit 1
-      fi
-
-      bash ./src/os/${distro}/setup.sh
-
-    else
-      print_error "${os} currently not supported."
-      exit 1
-    fi
+  else
+    print_error "${os} currently not supported."
+    exit 1
+  fi
 }
 
 main "$@"
--- src/os/Ubuntu/dep-Ubuntu.sh
+++ src/os/Ubuntu/dep-Ubuntu.sh
@@ -22,7 +22,7 @@ package_is_installed() {
     dpkg -s "$1" &>/dev/null
 }
 
-print_in_purple "\n• Utility\n\n"
+print_in_purple "\n • Utility\n\n"
 
 install_package "zsh" "zsh"
 install_package "cURL" "curl"
@@ -32,7 +32,7 @@ install_package "fzf" "fzf"
 install_package "fonts-powerline" "fonts-powerline"
 install_package "bat" "bat"
 
-print_in_purple "\n• Development\n\n"
+print_in_purple "\n • Development\n\n"
 
 install_package "Build Essential" "build-essential" # Install tools for compiling/building software from source.
 install_package "Git" "git"
@@ -42,7 +42,7 @@ curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash - && install_pac
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add - && echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list && sudo apt update && install_package "yarn" "yarn"
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg && sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null && sudo apt update && sudo apt install gh -y
 
-print_in_purple "\n• Docker\n\n"
+print_in_purple "\n • Docker\n\n"
 if ! cmd_exists "docker"; then
     curl -sSL https://get.docker.com | sh || error "Failed to install Docker."
     sudo usermod -aG docker $USER || error "Failed to add user to the Docker usergroup."
